ae5d1d7aee366126ea9e82b582b6b10b4d820811
Ido Berkovich <idoberko2@gmail.com>
2019-10-03 20:34:43 +0300

    CI Improvements

---
+++ .travis/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+eval "$(ssh-agent -s)";
+chmod 600 .travis/dokku-deploy.key;
+ssh-add .travis/dokku-deploy.key;
+ssh-keyscan $DOKKU_HOST >> ~/.ssh/known_hosts;
+git remote add deploy dokku@$DOKKU_HOST:$DOKKU_APP;
+git config --global push.default simple;
+git push deploy master;
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ./target/release/rust-hello-world
